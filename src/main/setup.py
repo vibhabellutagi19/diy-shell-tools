8007b0cef8c93185e50de736e189389e4ba40ee7
VibhavariBellutagi19 <vibhavari.bellutagi@gmail.com>
2024-02-10 14:52:03 +0100

    inital commit with basic setup

---
+++ src/main/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+def read_version():
+    with open("../../../VERSION", "r", encoding='utf-8') as version_file:
+        return version_file.read().strip().replace('-', '.')
+
+
+setup(
+    author="vibhavari bellutagi",
+    author_email="vibhavari.bellutagi@gmail.com",
+    description="""A dyi shell tools, mimics the shell commands like wc, grep, ls etc""",
+    name="dyi-shell-tools",
+    version=read_version()
+)
