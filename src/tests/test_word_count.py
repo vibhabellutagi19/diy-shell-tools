703fa2b18aae1f1fa9b1138011bd1876c3790df2
VibhavariBellutagi19 <vibhavari.bellutagi@gmail.com>
2024-02-11 14:38:46 +0100

    add tests for the wordcount

---
+++ src/tests/test_word_count.py
@@ -0,0 +1,28 @@
+import unittest
+
+from commands.command_factory import CommandsFactory
+
+
+class TestWordCount(unittest.TestCase):
+    def setUp(self):
+        """Set up for the test"""
+        self.wc_instance = CommandsFactory.create_command_instance('wc')
+
+    def test_valid_options(self):
+        """Test valid options for the wc command"""
+
+    def test_invalid_options(self):
+        """Test invalid options for the wc command"""
+
+    def test_options_c(self):
+        """Test the -c option: number of bytes in a file"""
+
+    def test_options_l(self):
+        """Test the -l option: number of lines in a file"""
+
+    def test_options_w(self):
+        """Test the -w option: number of words in a file"""
+
+    def test_for_default_options(self):
+        """Test for no options: no options are provided, which is the equivalent to the -c, -l and -w options """
+
