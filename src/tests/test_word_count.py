54ab0203567001531a8210078d0762bbae71b5ab
VibhavariBellutagi19 <vibhavari.bellutagi@gmail.com>
2024-02-11 15:24:19 +0100

    add validate_options method with tests

--- src/tests/test_word_count.py
+++ src/tests/test_word_count.py
@@ -1,19 +1,33 @@
 import unittest
 
 from commands.command_factory import CommandsFactory
+from commands.word_count import InvalidOptionError
 
 
 class TestWordCount(unittest.TestCase):
     def setUp(self):
         """Set up for the test"""
-        self.options = '-c'
-        self.wc_instance = CommandsFactory.create_command_instance('wc', self.options)
+        self.valid_options = ['-c']
+        self.invalid_options = ['-a']
+        self.invalid_wc_instance = CommandsFactory.create_command_instance('wc', self.invalid_options)
+        self.wc_instance = CommandsFactory.create_command_instance('wc', self.valid_options)
 
     def test_valid_options(self):
         """Test valid options for the wc command"""
 
+        try:
+            self.wc_instance.validate_options()
+        except Exception as e:
+            self.fail(f"Unexpected exception: {e}")
+        else:
+            self.assertTrue(True, "No exception raised for valid options")
+
     def test_invalid_options(self):
         """Test invalid options for the wc command"""
+        with self.assertRaises(InvalidOptionError) as context:
+            self.invalid_wc_instance.validate_options()
+        expection_message = f"Invalid option(s) {self.invalid_options}. Valid options are: -c, -l, -w"
+        self.assertEqual(expection_message, str(context.exception))
 
     def test_options_c(self):
         """Test the -c option: number of bytes in a file"""
@@ -26,4 +40,3 @@ class TestWordCount(unittest.TestCase):
 
     def test_for_default_options(self):
         """Test for no options: no options are provided, which is the equivalent to the -c, -l and -w options """
-
