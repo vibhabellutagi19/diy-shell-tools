a70d8be45995e7ebd4f65028d61a0d1631eadd48
VibhavariBellutagi19 <vibhavari.bellutagi@gmail.com>
2024-02-11 14:38:20 +0100

    add validate_options() method in base class as abtract method

--- src/main/commands/base_command.py
+++ src/main/commands/base_command.py
@@ -2,9 +2,10 @@ from abc import ABC, abstractmethod
 
 
 class BaseCommand(ABC):
-    def __init__(self, file_path: str):
-        self.input_file_path = file_path
+    @abstractmethod
+    def execute(self, file_path: str):
+        pass
 
     @abstractmethod
-    def execute(self):
+    def validate_options(self):
         pass
---
+++ src/main/commands/word_count.py
@@ -0,0 +1,7 @@
+from commands.base_command import BaseCommand
+
+
+class WordCount(BaseCommand):
+    def execute(self):
+        pass
+
